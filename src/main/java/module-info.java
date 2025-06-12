55c514634cecec2cd8dc6ec58b2cd76fff8fd003
Gabriel Carmo <gabrielcarmo1602@gmail.com>
2025-06-12 08:58:15 -0300

    fix: module-info-java exportando e abrindo todos os packeges

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,17 +2,22 @@ module com.example.projetosra3poo {
     requires javafx.controls;
     requires javafx.fxml;
 
-
     opens com.example.projetosra3poo to javafx.fxml;
     exports com.example.projetosra3poo;
 
-    opens projeto_gabriel to javafx.fxml;
-    exports projeto_gabriel;
+    opens projetoRA3 to javafx.graphics;
+    exports projetoRA3;
+
+    opens projeto_gabriel.controller to javafx.fxml;
+    opens projeto_gabriel.view to javafx.fxml;
+    opens projeto_gabriel.model to javafx.fxml;
+    exports projeto_gabriel.controller;
+    exports projeto_gabriel.model;
+    exports projeto_gabriel.view;
 
     opens projeto_igor to javafx.fxml;
     exports projeto_igor;
 
     opens projeto_lucas to javafx.fxml;
     exports projeto_lucas;
-
 }
